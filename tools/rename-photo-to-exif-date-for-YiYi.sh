a02e01faef581249775956250a3569423280454b
himacro <himacro@yeah.net>
2014-03-24 12:35:12 +0800

    Update the script for photo-rename

--- tools/rename-photo-to-exif-date-for-YiYi.sh
+++ tools/rename-photo-to-exif-date-for-YiYi.sh
@@ -1,4 +1,9 @@
-for f in *.JPG 
-    do new_fn=$(exiftool $f | grep 'Create Date' | sed 's/Create Date\s\+:/YiYi/' | sed 's/:/-/g')
+for f in *.JPG;
+do 
+    new_fn=$(exiftool $f | grep 'Create Date' | sed 's/Create Date\s\+:/YiYi/' | sed 's/:/-/g')
+    if [ -e new_fn ]
+    then
+        new_fn = ${new_fn}-2
+    fi
     cp -v "$f" "$new_fn.JPG"
 done
