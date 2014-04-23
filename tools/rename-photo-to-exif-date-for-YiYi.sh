70147516b212b0ad5e43cfc26d52e518faad4b78
himacro <himacro@yeah.net>
2014-04-23 11:15:01 +0800

    Update the script to rename photos for YiYi

--- tools/rename-photo-to-exif-date-for-YiYi.sh
+++ tools/rename-photo-to-exif-date-for-YiYi.sh
@@ -1,9 +1,9 @@
 for f in *.JPG;
 do 
     new_fn=$(exiftool $f | grep 'Create Date' | sed 's/Create Date\s\+:/YiYi/' | sed 's/:/-/g')
-    if [ -e new_fn ]
+    if [[ -e ${new_fn}.JPG ]]
     then
-        new_fn = ${new_fn}-2
+        new_fn=${new_fn}-2
     fi
     cp -v "$f" "$new_fn.JPG"
 done
